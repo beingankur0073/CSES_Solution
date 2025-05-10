c54fdc2fda1a46225b2225c8fa26d33c22499ed1
Ankur Mukherjee <mukherjeeankur0073@gmail.com>
2025-05-10 17:54:44 +0530

    Add solutions for Coin Piles and Two Knights problems

---
+++ Introductory/Coin_Piles.cpp
@@ -0,0 +1,79 @@
+//For cp website format
+ 
+#include<bits/stdc++.h>
+#include<chrono>
+using namespace std;
+ 
+#define int long long
+#define mp  make_pair
+#define endl "\n"
+#define MAX LLONG_MAX
+#define MIN LLONG_MIN
+#define pb push_back
+#define umap unordered_map
+#define uset unordered_set
+#define minel *min_element
+#define maxel *max_element
+#define vi vector<int>
+#define vstr vector<string>
+#define vpr vector<pair<int,int>>
+#define ceil(x,y) ceil((double)x/(double)y)
+ 
+const int MOD=1e9+7;
+vi prim;
+ 
+ 
+const int N=1e5+1; //max constraint 
+vi divisors[N];
+ 
+int binexp(int a,int b){int res=1;while(b>0){if(b&1){res=(res*1ll*a)%MOD;}a=(a*1LL*a)%MOD;b>>=1;} return res;}
+bool isprime(int n){if(n==1) return 0;for(int i=2;i*i<=n;i++){if(n%i==0) return 0;}return 1;}
+void prim_siv(int n){bool primes[n+1];fill(primes,primes+n+1,true);primes[0]=primes[1]=false;for(int i=2;i*i<=n;i++){if(primes[i]){for(int j=i*i;j<=n;j+=i){primes[j]=false;}}}for(int i=2;i<n+1;i++){if(primes[i]==true) prim.push_back(i);}}
+void divfind() { for(int i=1; i<N; i++) { divisors[i].push_back(i); for(int j=2*i; j<N; j+=i) { divisors[j].push_back(i); } } }
+ 
+ 
+ 
+ 
+ 
+void solve(){
+     int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        // this is a problem of 2 linear equations
+        // a=2x+y b=x+2y x operations(21) and y operations(12)
+        // solving them results in
+        // x=(2a-b)/3 y=(2b-a)/3
+        int x=(2*a-b);
+        int y=(2*b-a);
+        if(x>=0&&y>=0&&x%3==0&&y%3==0){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+    }
+ 
+}
+ 
+signed main(){
+    auto start = chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+	cin.tie(0);
+    cout.tie(0);
+    solve();
+ 
+    auto end = chrono::high_resolution_clock::now();
+    double time_taken = 
+      chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+ 
+    time_taken *= 1e-9;
+ 
+    // cout << "Time taken by program is : " << fixed 
+    //      << time_taken << setprecision(9)<<"sec"<<endl;
+         
+    
+}
+ 
+/**
+ * ? run loop multiple times incase of prime factorisations
+*/
